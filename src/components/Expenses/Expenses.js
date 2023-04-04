a9cdeac3aa2d0fcc2d0803fb11a20be250e67629
Ahmed Osama Sawah <122928535+Ahmed-Sawah@users.noreply.github.com>
2023-04-04 08:06:59 +0200

    Change starting year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import ExpenseList from "./ExpensesList";
 import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2023");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
